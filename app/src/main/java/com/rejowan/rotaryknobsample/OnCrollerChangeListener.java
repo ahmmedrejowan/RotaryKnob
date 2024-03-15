25e9b1842b74ee79c0d3778c406b6892d2f29acd
ahmmedrejowan <ahmmedrejowan@gmail.com>
2024-03-15 20:09:26 +0600

    Cleanup

--- app/src/main/java/com/rejowan/rotaryknobsample/OnCrollerChangeListener.java
+++
@@ -1,9 +0,0 @@
-package com.rejowan.rotaryknobsample;
-
-public interface OnCrollerChangeListener {
-    void onProgressChanged(Croller croller, int progress);
-
-    void onStartTrackingTouch(Croller croller);
-
-    void onStopTrackingTouch(Croller croller);
-}
--- app/src/main/java/com/rejowan/rotaryknobsample/OnCrollerChangeListener2.java
+++
@@ -1,9 +0,0 @@
-package com.rejowan.rotaryknobsample;
-
-public interface OnCrollerChangeListener2 {
-    void onProgressChanged(Croller2 croller, int progress);
-
-    void onStartTrackingTouch(Croller2 croller);
-
-    void onStopTrackingTouch(Croller2 croller);
-}
