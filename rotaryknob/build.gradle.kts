4467947c639b49e466fd8e5c244229434630da77
ahmmedrejowan <ahmmedrejowan@gmail.com>
2024-03-18 05:22:29 +0600

    Jitpack Deploy

--- rotaryknob/build.gradle.kts
+++ rotaryknob/build.gradle.kts
@@ -1,3 +1,7 @@
+apply {
+    plugin("maven-publish")
+}
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -34,10 +38,22 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.rejowan"
+            artifactId = "rotaryknob"
+            version = "0.1.0"
+            afterEvaluate { artifact(tasks.getByName("bundleReleaseAar"))
+            }
+        }
+    }
 }
